bdb3ce7c5ed4b15c00495a00e10b42c1179ea5bb
colymba <thierry@colymba.com>
2012-07-20 21:02:45 +0300

    API: Added config vars to component
    Switching all component settings to a config variable with getter and
    setter

--- code/GridFieldBulkImageUpload.php
+++ code/GridFieldBulkImageUpload.php
@@ -19,6 +19,18 @@ class GridFieldBulkImageUpload implements GridField_HTMLProvider, GridField_URLH
 	protected $recordEditableFields;
 	
 	/**
+	 * component configuration
+	 * 
+	 * @var array 
+	 */
+	protected $config = array(
+		'imageFieldName' => null,
+		'editableFields' => null,
+		'fieldsClassBlacklist' => array( 'GridField', 'UploadField' ),
+		'fieldsNameBlacklist' => array()
+	);
+
+	/**
 	 * 
 	 * @param string $imageField
 	 * @param string/array $editableFields 
@@ -29,6 +41,82 @@ class GridFieldBulkImageUpload implements GridField_HTMLProvider, GridField_URLH
 				
 		if ( !is_array($editableFields) && $editableFields != null ) $editableFields = array($editableFields);
 		$this->setRecordEditableFields($editableFields);
+	}
+	
+	function setConfig ( $reference, $value )
+	{
+		if ( isset( $this->config[$reference] ) )
+		{
+			if ( ($reference == 'fieldsClassBlacklist' || $reference == 'fieldsClassBlacklist') && !is_array($value) )
+			{
+				$value = array($value);
+			}
+			$this->$config[$reference] = $value;
+		}
+	}
+	
+	/**
+	 * Returns one $config parameter of the full $config
+	 * 
+	 * @param string $reference $congif parameter to return
+	 * @return mixed 
+	 */
+	function getConfig ( $reference = false )
+	{
+		if ( $reference ) return $this->config[$reference];
+		else return $this->config;
+	}
+	
+	/**
+	 * Add a field to the editable fields blacklist
+	 * 
+	 * @param string $fieldName
+	 * @return boolean 
+	 */
+	function addFieldNameToBlacklist ( $fieldName )
+	{
+		return array_push( $this->config['fieldsNameBlacklist'], $fieldName);
+	}
+	
+	/**
+	 * Add a class to the editable fields blacklist
+	 * 
+	 * @param string $className
+	 * @return boolean 
+	 */
+	function addClassToBlacklist ( $className )
+	{
+		return array_push( $this->config['fieldsClassBlacklist'], $className);
+	}
+	
+	/**
+	 * Remove a field to the editable fields blacklist
+	 * 
+	 * @param string $fieldName
+	 * @return boolean 
+	 */
+	function removeFieldNameFromBlacklist ( $fieldName )
+	{
+		if (key_exists($fieldName, $this->config['fieldsNameBlacklist'])) {
+			return delete( $this->config['fieldsNameBlacklist'][$fieldName] );
+		}else{
+			return false;
+		}
+	}
+	
+	/**
+	 * Remove a class to the editable fields blacklist
+	 * 
+	 * @param string $className
+	 * @return boolean 
+	 */
+	function removeClassFromBlacklist ( $className )
+	{
+		if (key_exists($className, $this->config['fieldsNameBlacklist'])) {
+			return delete( $this->config['fieldsNameBlacklist'][$className] );
+		}else{
+			return false;
+		}
 	}
 	
 	/**
