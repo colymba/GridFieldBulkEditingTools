aaaca90a93f2350dcf288639d0b4a890f6cab86a
Thierry François <thierry@colymba.com>
2014-09-06 14:37:31 +0300

    Pull transifex translations

--- lang/js/de.js
+++ lang/js/de.js
@@ -4,7 +4,7 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
   if(typeof(console) != 'undefined') console.error('Class ss.i18n not defined');
 } else {
   ss.i18n.addDictionary('de', {
-    "GRIDFIELD_BULK_UPLOAD.PROGRESS_INFO": "Uploading %s file(s). %s done. %s error(s).",
+    "GRIDFIELD_BULK_UPLOAD.PROGRESS_INFO": "Übertrage %s Datei(en). %s erfolgreich übertragen. %s Fehler.",
     "GRIDFIELD_BULK_MANAGER.BULKACTION_EMPTY_SELECT": "Sie müssen mindestens ein Element auswählen.",
     "GRIDFIELD_BULK_MANAGER.CONFIRM_DESTRUCTIVE_ACTION": "Wollen Sie dieses Element wirklich unwiderruflich löschen?"
 });
