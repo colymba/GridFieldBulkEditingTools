55adbcb63c49444268f04f2d5676b0ce36c12721
James Cocker <purplespider@users.noreply.github.com>
2023-06-01 10:46:37 +0100

    SS5 Fix: Replace json2array with json_decode

--- src/BulkUploader/BulkUploadHandler.php
+++ src/BulkUploader/BulkUploadHandler.php
@@ -114,7 +114,7 @@ class BulkUploadHandler extends RequestHandler
         
         if ($uploadResponse->getStatusCode() == 200)
         {
-            $responseData = Convert::json2array($uploadResponse->getBody());
+            $responseData = json_decode($uploadResponse->getBody(), true);
             $responseData = array_shift($responseData);
 
             $record = $this->createDataObject($responseData['id']);
